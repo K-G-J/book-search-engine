a7ccb6caa1fcde9e99391c028fed3ebaf249bd35
Kate Johnson <johnson.kat.g@gmail.com>
2022-04-27 22:50:40 -0400

    redfactoring for bug fixes

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,50 +1,58 @@
-import React from 'react';
-import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
+import React from 'react'
+import {
+  Jumbotron,
+  Container,
+  CardColumns,
+  Card,
+  Button,
+} from 'react-bootstrap'
 
-import Auth from '../utils/auth';
-import { removeBookId } from '../utils/localStorage';
+import Auth from '../utils/auth'
+import { removeBookId } from '../utils/localStorage'
 
 import { useMutation, useQuery } from '@apollo/client'
 import { REMOVE_BOOK } from '../utils/mutations'
-import { QUERY_ME } from '../utils/queries'
+import { GET_ME } from '../utils/queries'
 
 const SavedBooks = () => {
-  const { loading, data } = useQuery(QUERY_ME)
-  const [deleteBook] = useMutation(REMOVE_BOOK)
-  
+  const { data } = useQuery(GET_ME)
+  const [deleteBook] = useMutation(REMOVE_BOOK, {
+    update(cache, { data: { deleteBook } }) {
+      try {
+        const { me } = cache.readQuery({ query: GET_ME })
+        const updatedBookCache = me.savedBooks.filter( (book) => book.Id !== deleteBook, )
+        me.savedBooks = updatedBookCache
+        cache.writeQuery({ query: GET_ME, data: { me: { ...me.savedBooks } } })
+      } catch (e) {
+        console.error(e)
+      }
+    },
+  })
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
-    const token = Auth.loggedIn() ? Auth.getToken() : null;
+    const token = Auth.loggedIn() ? Auth.getToken() : null
 
     if (!token) {
-      return false;
+      return false
     }
 
     try {
       await deleteBook({
         variables: { bookId: bookId },
-        update: cache => {
-          const { me } = cache.readQuery({ query: QUERY_ME })
-          const savedBooksCache = me.savedBooks;
-          const updatedBookCache = savedBooksCache.filter(book => book.Id !== bookId);
-          me.savedBooks = updatedBookCache
-          cache.writeQuery({ query: QUERY_ME, data: { me: { ...me.savedBooks } } })
-          
-        }
       })
       // upon success, remove book's id from localStorage
-      removeBookId(bookId);
+      removeBookId(bookId)
     } catch (err) {
-      console.error(err);
+      console.error(err)
     }
-  };
+  }
 
   const userData = data?.me || {}
 
   if (!userData.username) {
     return (
-        <h4>
+      <h4>
         You need to be logged in to see this page. Use the navigation links
         above to sign up or log in!
       </h4>
@@ -53,12 +61,12 @@ const SavedBooks = () => {
 
   // if data isn't here yet, say so
   if (!userData.length) {
-    return <h2>LOADING...</h2>;
+    return <h2>LOADING...</h2>
   }
 
   return (
     <>
-      <Jumbotron fluid className='text-light bg-dark'>
+      <Jumbotron fluid className="text-light bg-dark">
         <Container>
           <h1>Viewing saved books!</h1>
         </Container>
@@ -66,29 +74,40 @@ const SavedBooks = () => {
       <Container>
         <h2>
           {userData.savedBooks.length
-            ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? 'book' : 'books'}:`
+            ? `Viewing ${userData.savedBooks.length} saved ${
+                userData.savedBooks.length === 1 ? 'book' : 'books'
+              }:`
             : 'You have no saved books!'}
         </h2>
         <CardColumns>
           {userData.savedBooks.map((book) => {
             return (
-              <Card key={book.bookId} border='dark'>
-                {book.image ? <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' /> : null}
+              <Card key={book.bookId} border="dark">
+                {book.image ? (
+                  <Card.Img
+                    src={book.image}
+                    alt={`The cover for ${book.title}`}
+                    variant="top"
+                  />
+                ) : null}
                 <Card.Body>
                   <Card.Title>{book.title}</Card.Title>
-                  <p className='small'>Authors: {book.authors}</p>
+                  <p className="small">Authors: {book.authors}</p>
                   <Card.Text>{book.description}</Card.Text>
-                  <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book.bookId)}>
+                  <Button
+                    className="btn-block btn-danger"
+                    onClick={() => handleDeleteBook(book.bookId)}
+                  >
                     Delete this Book!
                   </Button>
                 </Card.Body>
               </Card>
-            );
+            )
           })}
         </CardColumns>
       </Container>
     </>
-  );
-};
+  )
+}
 
-export default SavedBooks;
+export default SavedBooks
--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -15,7 +15,7 @@ import { saveBookIds, getSavedBookIds } from '../utils/localStorage'
 
 import { useMutation } from '@apollo/client'
 import { SAVE_BOOK } from '../utils/mutations'
-import { QUERY_ME } from '../utils/queries'
+import { GET_ME } from '../utils/queries'
 
 const SearchBooks = () => {
   // create state for holding returned google api data
@@ -26,7 +26,21 @@ const SearchBooks = () => {
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds())
 
-  const [saveBook] = useMutation(SAVE_BOOK)
+  const [saveBook] = useMutation(SAVE_BOOK, {
+    update(cache, { data: { saveBook } }) {
+      try {
+        const { me } = cache.readQuery({ query: GET_ME })
+        cache.writeQuery({
+          query: GET_ME,
+          data: {
+            me: { ...me, savedBooks: [...me.savedBoooks, saveBook] },
+          },
+        })
+      } catch (e) {
+        console.error(e)
+      }
+    },
+  })
 
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
@@ -81,15 +95,6 @@ const SearchBooks = () => {
     try {
       await saveBook({
         variables: { book: bookToSave },
-        update: (cache) => {
-          const { me } = cache.readQuery({ query: QUERY_ME })
-          cache.writeQuery({
-            query: QUERY_ME,
-            data: {
-              me: { ...me, savedBooks: [...me.savedBoooks, bookToSave] },
-            },
-          })
-        },
       })
 
       // if book successfully saves to user's account, save book id to state
