ea43660575a40ed19c06ecd9389e6038623ef5ce
Kate Johnson <johnson.kat.g@gmail.com>
2022-05-09 05:09:25 -0400

    added new build

--- server/server.js
+++ server/server.js
@@ -31,8 +31,8 @@ app.get('*', (_req, res) => {
 
 db.once('open', () => {
   app.listen(PORT, () => {
-    console.log(`🌍 Now listening on localhost:${PORT}`)
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`)
+    console.log(`📚 Now listening on localhost:${PORT}`)
+    console.log(`🚀 Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`)
   });
 
 });
